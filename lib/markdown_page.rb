73e234b659d8826b3e36cf533fd847fbc2c7ea4b
Alex Chaffee <alex@stinky.com>
2011-10-14 18:03:42 -0700

    more file moving

--- lib/markdown_page.rb
+++ lib/markdown_page.rb
@@ -1,21 +1,20 @@
 require 'erector'
-require "./github_flavored_markdown"
-
+require "github_flavored_markdown"
 
 class MarkdownPage < Erector::Widget
   include GithubFlavoredMarkdown
-  
+
   def md2html(md)
     Markdown.new(gfm(md)).to_html
   end
-  
+
   needs :case_name, :doc_title, :markdown_src
   attr_reader :case_name, :doc_title, :markdown_src
-  
+
   def case_title
     "Railsbridge #{case_name.capitalize}"
   end
-  
+
   def content
     head {
       title "#{doc_title} - #{case_title}"
@@ -29,7 +28,7 @@ class MarkdownPage < Erector::Widget
         -webkit-margin-before: 0;
         -webkit-margin-after: 0;
         -webkit-margin-start: 0;
-        -webkit-margin-end: 0;            
+        -webkit-margin-end: 0;
       }
 
       .top {
@@ -48,7 +47,7 @@ class MarkdownPage < Erector::Widget
 
       .bottom {
         min-height: 60px;
-        
+
       }
 
       .toc {
@@ -68,9 +67,9 @@ class MarkdownPage < Erector::Widget
         padding: .5em .5em .25em;
         margin-bottom: .25em;
         margin-left: -2em;
-      }          
-      
-      .doc { 
+      }
+
+      .doc {
         max-width: 50em;
       }
       .doc pre {
@@ -98,11 +97,11 @@ class MarkdownPage < Erector::Widget
           rawtext md2html(markdown_src)
         }
       }
-      
+
       div(:class=>:bottom) {
-        
+
       }
-      
+
     }
   end
 end
