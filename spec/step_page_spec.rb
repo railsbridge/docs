93848bc19bfd39970cfa1401935acc5e81c992a7
Alex Chaffee <alex@stinky.com>
2014-02-12 11:35:30 -0800

    use rspec double() instead of deprecated mock()

--- spec/step_page_spec.rb
+++ spec/step_page_spec.rb
@@ -18,7 +18,7 @@ describe StepPage do
     )
 
     # this is a hack to make the TOC work in the absence of a real site
-    Site.should_receive(:named).and_return(mock(dir: "/tmp"))
+    Site.should_receive(:named).and_return(double(dir: "/tmp"))
 
     html_doc = Nokogiri.parse(page.to_html)
     main_html = html_doc.css("main").first.serialize(:save_with => 0).chomp
