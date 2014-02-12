7f32f0ebfa6d769579e2b5d94675d3acd59a182a
Alex Chaffee <alex@stinky.com>
2014-02-12 11:14:24 -0800

    clean up syntax error messages

--- spec/site_syntax_spec.rb
+++ spec/site_syntax_spec.rb
@@ -12,19 +12,26 @@ describe "Syntax check all sites" do
     @app ||= InstallFest.new
   end
 
-  def get! *args
-    get *args
-    assert { last_response.status == 200 }
-  end
-
   ['en', 'es'].each do |locale|
     describe "in locale '#{locale}'" do
       Site.all(locale).each do |site|
         describe "#{site.name} pages..." do
           site.docs.each do |doc|
             it "renders #{doc.filename}" do
-              get! "/#{site.name}/#{doc.name}", locale: locale
-              assert { last_response.ok? }
+              path = "/#{site.name}/#{doc.name}"
+              get path, locale: locale
+              if (last_response.status != 200)
+                errors = last_response.errors
+                meaningful_errors = errors.each_line.select do |l|
+                  l.include? path or l !~ %r{^\t}
+                end.join
+                puts "#{path}: #{meaningful_errors}"
+                fail("Syntax errors in #{path} -- see above")
+              end
+
+              last_response_status = last_response.status
+              assert { last_response_status == 200 }
+
               if doc.filename.end_with?('.step')
                 assert { last_response.body !~ /FUZZY/ }
               end
