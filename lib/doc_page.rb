73146c39eddfb31e842c759769dd8746d1bedccd
Davey Shafik <me@daveyshafik.com>
2014-05-01 20:23:19 -0400

    Add google analytics

--- lib/doc_page.rb
+++ lib/doc_page.rb
@@ -161,6 +161,16 @@ class DocPage < Erector::Widgets::Page
           img.ccby :src => "/img/cc-by.png"
         }
       end
+
+      javascript <<-js
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-50584404-2', 'phpbridge.org');
+        ga('send', 'pageview');
+      js
     }
   end
 
